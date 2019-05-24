b0749a5d5b340619add77e898929e7832ab465ac
Paul Sarando <psarando@gmail.com>
2019-05-24 07:43:26 -0700

    Add build assets.

---
+++ examples/precache-manifest.58a9070f96f898198eceeaf1f5260e5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19741831c78e0b1929300810e1689f3d",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.489aea7468ce565022f0.bundle.js"
+  },
+  {
+    "url": "runtime~main.489aea7468ce565022f0.bundle.js"
+  },
+  {
+    "revision": "6d6fbcc6eb29eeb890f3",
+    "url": "static/css/main.31d92830.chunk.css"
+  },
+  {
+    "url": "vendors~main.489aea7468ce565022f0.bundle.js"
+  },
+  {
+    "url": "vendors~main.489aea7468ce565022f0.bundle.js.LICENSE.txt"
+  }
+]);
---
+++ precache-manifest.c7d2761927219e1beee6e92cddf468a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "388b810931bfd7457d239b21b9688172",
+    "url": "/shire-reckoning/../_includes/header.html"
+  },
+  {
+    "revision": "982d8221e710bb68c2502d9394d76beb",
+    "url": "/shire-reckoning/index.html"
+  },
+  {
+    "revision": "0f154f8c6ffd66a8ad51",
+    "url": "/shire-reckoning/static/css/main.31d92830.chunk.css"
+  },
+  {
+    "revision": "6a43c47aa9e3f460cd36",
+    "url": "/shire-reckoning/static/js/2.722b7d01.chunk.js"
+  },
+  {
+    "revision": "89d3385f7d39af76bd84ec1c3fe43df2",
+    "url": "/shire-reckoning/static/js/2.722b7d01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f154f8c6ffd66a8ad51",
+    "url": "/shire-reckoning/static/js/main.1f355418.chunk.js"
+  },
+  {
+    "revision": "a7e1a2ec594085568bf0",
+    "url": "/shire-reckoning/static/js/runtime~main.f576be14.js"
+  }
+]);
