fe532de4107a550e347063ffd8afc5f166b680a6
Paul Sarando <psarando@gmail.com>
2023-03-25 05:00:00 -0700

    Happy Yestarë!
    Add webpack.app.config.js and build script (mainly for gh-pages).
    Fix export from app index.js (broken after Storybook v6 upgrade).
    Also no need for PUBLIC_URL anymore (was from create-react-app).

--- src/index.js
+++ src/index.js
@@ -4,11 +4,7 @@ import ReactDOM from "react-dom";
 window.React = React;
 window.ReactDOM = ReactDOM;
 
-export {
-    default as TolkienCalendarsExample,
-} from "./examples/TolkienCalendars";
-export {
-    default as SimulatedTolkienCalendars,
-} from "./examples/simulation/TolkienCalendars";
+export { TolkienCalendarsExample } from "./examples/TolkienCalendars";
+export { SimulatedTolkienCalendars } from "./examples/simulation/TolkienCalendars";
 
 export * from "./lib";
---
+++ webpack.app.config.js
@@ -0,0 +1,90 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+const config = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "build"),
+        // There will be one main bundle, and one file per asynchronous chunk.
+        filename: "static/js/[name].[contenthash:8].js",
+        // There are also additional JS chunk files if you use code splitting.
+        chunkFilename: "static/js/[name].[contenthash:8].chunk.js",
+        publicPath: ".",
+        library: "TolkienCalendars",
+        libraryTarget: "umd",
+    },
+    // Stop compilation early in production
+    bail: true,
+    devtool: "source-map",
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                cache: true,
+                sourceMap: true,
+            }),
+            new CssMinimizerPlugin(),
+        ],
+        runtimeChunk: "single",
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: "vendors",
+                    chunks: "all",
+                },
+            },
+        },
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: "head",
+            template: "public/index.html",
+            minify: {
+                removeRedundantAttributes: true,
+                useShortDoctype: true,
+                removeEmptyAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                keepClosingSlash: true,
+                minifyJS: true,
+                minifyCSS: true,
+                minifyURLs: true,
+            },
+        }),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "static/css/[name].[contenthash:8].css",
+            chunkFilename: "static/css/[name].[contenthash:8].chunk.css",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: "babel-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: {
+                            importLoaders: 1,
+                            sourceMap: true,
+                        },
+                    },
+                    "postcss-loader",
+                ],
+            },
+        ],
+    },
+};
+
+module.exports = config;
